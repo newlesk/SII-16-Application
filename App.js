953ce07f14ef1d9a15bbff2b592ed001b56e2745
Krittipong Wachirangkul <34181035+newlesk@users.noreply.github.com>
2023-06-27 21:47:33 +0700

    update

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
+import React, {useState} from 'react';
+import Lenny from "./assets/Lenny.png"
 
 export default function App() {
+
+  const [titleText, setTitleText] = useState("Bird's Nest");
+  const bodyText = 'This is not really a bird nest.';
+
+  const onPressTitle = () => {
+    setTitleText("Bird's Nest [pressed]");
+  };
+  
   return (
-    <View style={styles.container}>
+    <View style={styles.container}> 
+
+    <View style={{backgroundColor:"blue",flex:0.5}}> 
+    <Image source={Lenny}/>
+
+    <Text onPress={onPressTitle}> New</Text>
+    <Text>{titleText}</Text>
+
+    </View>
+
+
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
